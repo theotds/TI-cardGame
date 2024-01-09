cdcf154d4749e68003d608cba8458138e1b3cfe5
theotds <theo.deschaeck@gmail.com>
2024-01-09 18:37:05 +0100

    delete unecessery print

--- src/main/java/Rooms/RoomManager.java
+++ src/main/java/Rooms/RoomManager.java
@@ -65,7 +65,6 @@ public class RoomManager {
     }
 
     public boolean doesRoomExist(String roomId) {
-        System.out.println(rooms.containsKey(roomId));
         return rooms.containsKey(roomId);
     }
 
